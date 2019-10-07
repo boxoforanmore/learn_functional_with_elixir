5e77faf5a2dac2692f514bcc57c345b11e64d1a4
Peter Nielson <computergeekpete@gmail.com>
2019-10-07 16:14:03 -0600

    Changed to add tail recursion for better memory efficiency

--- lib/ch4/factorial.ex
+++ lib/ch4/factorial.ex
@@ -1,6 +1,10 @@
 defmodule Factorial do
-    def of(0), do: 1
-    def of(n) when n > 0 do
-        n * of(n - 1)
+    ## Now with Tail Recursion
+
+    def of(n), do: factorial_of(n, 1)
+
+    def factorial_of(0, acc), do: acc
+    def factorial_of(n, acc) when n > 0 do
+        factorial_of(n - 1, n * acc)
     end
 end
