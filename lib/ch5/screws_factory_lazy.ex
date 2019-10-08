43a6976896ec7a724c5e760ace7fa67df959aa01
Peter Nielson <computergeekpete@gmail.com>
2019-10-08 14:22:56 -0600

    Added chunking to speed up the screw factory even more

--- lib/ch5/screws_factory_lazy.ex
+++ lib/ch5/screws_factory_lazy.ex
@@ -1,22 +1,24 @@
 defmodule ScrewsFactory do
 
-    ### Lazy evaluation example
+    ### Chunking and Lazy evaluation example
 
     def run(pieces) do
         pieces
-        |> Stream.map(&add_thread/1)
-        |> Stream.map(&add_head/1)
+        |> Stream.chunk(50)
+        |> Stream.flat_map(&add_thread/1)
+        |> Stream.chunk(100)
+        |> Stream.flat_map(&add_head/1)
         |> Enum.each(&output/1)
     end
 
-    defp add_thread(piece) do
+    defp add_thread(pieces) do
         Process.sleep(50)
-        piece <> "--"
+        Enum.map(pieces, &(&1 <> "--"))
     end
 
-    defp add_head(piece) do
+    defp add_head(pieces) do
         Process.sleep(100)
-        "0" <> piece
+        Enum.map(pieces, &("o" <> &1))
     end
 
     defp output(screw) do
