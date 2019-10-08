49bd576b19d3426a6c1b6d7e9dfb315451ccc6da
Peter Nielson <computergeekpete@gmail.com>
2019-10-08 13:38:01 -0600

    Added infinite stream for factorial function

--- lib/ch5/factorial.ex
+++ lib/ch5/factorial.ex
@@ -1,7 +1,7 @@
 defmodule Factorial do
     def of(0), do: 1
     def of(n) when n > 0 do
-        1..10_000_000
+        Stream.iterate(1, &(&1 + 1))
             |> Enum.take(n)
             |> Enum.reduce(&(&1 * &2))
     end
