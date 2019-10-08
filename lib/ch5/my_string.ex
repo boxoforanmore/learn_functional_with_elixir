09abb5e4e82fc264dbdc8d026554563b9f64a9d9
Peter Nielson <computergeekpete@gmail.com>
2019-10-07 18:05:59 -0600

    Changed capitalize_words to chain in a confusing manner

--- lib/ch5/my_string.ex
+++ lib/ch5/my_string.ex
@@ -1,7 +1,11 @@
 defmodule MyString do
     def capitalize_words(title) do
-        words = String.split(title)
-        capitalized_words = Enum.map(words, &String.capitalize/1)
-        Enum.join(capitalized_words, " ")
+        Enum.join(
+            Enum.map(
+                String.split(title),
+                &String.capitalize/1
+            ), 
+            " "
+        )
     end
 end
